a429c4c9e7531c44589a6922849b57332eeb2aed
Kim <kmora2b@gmail.com>
2020-08-31 16:08:53 -0600

    Regex strip of punctuation and split by space

--- wordCount.py
+++ wordCount.py
@@ -1,7 +1,27 @@
-import re 
+import re, sys
 
 word_dict = {}
 
-f = open("speech.txt", "r")
-print(f.read())
-f.close() 
+input_file = open(sys.argv[1], "r")
+word_string = input_file.read()
+input_f.close() 
+
+#TEST
+#print(word_string)
+
+proc_strlist = re.sub(r'[^\w\s]','',word_string.lower() ).split()
+proc_strlist.sort()
+
+for s in proc_strlist:
+    if s not in word_dict:
+        word_dict[s] = 1
+    else:
+        word_dict[s] += 1
+#TEST
+#print(word_dict)
+
+for k in word_dict:
+    print(k + " " + str(word_dict[k]))
+
+
+
