259f7732d90201341143a3e0c04b0da924f0d29b
Kim <kmora2b@gmail.com>
2020-09-01 23:37:46 -0600

    Added file write in feature and fixed .sub punctuation replacement

--- wordCount.py
+++ wordCount.py
@@ -4,15 +4,17 @@ word_dict = {}
 
 input_file = open(sys.argv[1], "r")
 word_string = input_file.read()
-input_f.close() 
+input_file.close() 
 
 #TEST
 #print(word_string)
 
-proc_strlist = re.sub(r'[^\w\s]','',word_string.lower() ).split()
+proc_strlist = re.sub(r'[^\w\s]',' ',word_string.lower() ).split()
 proc_strlist.sort()
+print(proc_strlist)
 
 for s in proc_strlist:
+    print(s)
     if s not in word_dict:
         word_dict[s] = 1
     else:
@@ -20,8 +22,14 @@ for s in proc_strlist:
 #TEST
 #print(word_dict)
 
+output_file = open(sys.argv[1][:len(sys.argv[1])-4] + "Output.txt", "w")
+
+
 for k in word_dict:
-    print(k + " " + str(word_dict[k]))
+    output_file.write(k + " " + str(word_dict[k]) + "\n")
+
+output_file.close()
+
 
 
 
