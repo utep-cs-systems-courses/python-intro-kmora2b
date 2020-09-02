a33ffad817af64581a7c6c6896d29bf033ba2061
Kim <kmora2b@gmail.com>
2020-09-02 00:00:56 -0600

    Modularize code

--- wordCount.py
+++ wordCount.py
@@ -1,35 +1,57 @@
-import re, sys
+import re, sys, os
 
-word_dict = {}
+# Check if cmd line args are inputted
+def check_args_values():
+    if len(sys.argv) is not 2:
+        print("Correct usage: wordCount.py <input text file>")
+        exit()
 
-input_file = open(sys.argv[1], "r")
-word_string = input_file.read()
-input_file.close() 
+# Check if the file exits
+def check_file_exists(fname):
+    if not os.path.exists(fname):
+        print ("File %s doesn't exist! Exiting" % fname)
+        exit()
 
-#TEST
-#print(word_string)
+# Read in file
+def read_file(fname):
+    input_file = open(fname, "r")
+    word_string = input_file.read()
+    input_file.close() 
+    return word_string
 
-proc_strlist = re.sub(r'[^\w\s]',' ',word_string.lower() ).split()
-proc_strlist.sort()
-print(proc_strlist)
+# Strip string of punctuation and create sorted dictionary
+# dictionary structure: {word : count}
+def process_str(string):
+    proc_strlist = re.sub(r'[^\w\s]',' ',string.lower() ).split()
+    proc_strlist.sort()
 
-for s in proc_strlist:
-    print(s)
-    if s not in word_dict:
-        word_dict[s] = 1
-    else:
-        word_dict[s] += 1
-#TEST
-#print(word_dict)
+    for s in proc_strlist:
+        if s not in word_dict:
+            word_dict[s] = 1
+        else:
+            word_dict[s] += 1
+    return word_dict
 
-output_file = open(sys.argv[1][:len(sys.argv[1])-4] + "Output.txt", "w")
+# Write dictionary values line by line 
+def write_into_file(fname, word_dict):
+    output_file = open( fname[ : len(fname)-4] + "Output.txt", "w")
 
+    for k in word_dict:
+        output_file.write(k + " " + str(word_dict[k]) + "\n")
 
-for k in word_dict:
-    output_file.write(k + " " + str(word_dict[k]) + "\n")
+    output_file.close()
 
-output_file.close()
+if __name__ == "__main__":
+    word_dict = {}
+    check_args_values()
 
+    fname = sys.argv[1]
+    check_file_exists(fname)
 
+    word_string = read_file(fname)
+    word_dict = process_str(word_string)
+    write_into_file(fname, word_dict)
+
+    print("File creation successful")
 
 
